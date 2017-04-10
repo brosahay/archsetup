ef1bc3750d506d9597dcc2ea1072045bf460b728
revosftw <sahay.rohan@gmail.com>
2017-04-11 01:36:47 +0530

    inprogress

---
+++ arch-common-install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pacstrap /mnt base base-devel xorg-server xorg-server-utils xorg-xinit xorg-xrandr xorg-drivers dialog wpa_supplicant sudo bash-completion wget dkms net-tools xdg-user-dirs grub os-prober
--- budgie-minimal-install.sh
+++ budgie-minimal-install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-pacstrap /mnt base base-devel xorg-server xorg-server-utils xorg-xinit xorg-xrandr xorg-drivers gdm gnome-themes-standard gnome-session gnome-shell-extensions dkms 
-net-tools xdg-user-dirs sudo bash-completion wget grub os-prober baobab evince file-roller firefox gedit gnome-backgrounds gnome-calculator gnome-control-center 
-gnome-screenshot gnome-system-monitor gnome-terminal gnome-tweak-tool gstreamer0.10-plugins mutter nautilus noise vala viewnior vlc
-arch-chroot /mnt
+pacstrap /mnt 
+gdm gnome-themes-standard gnome-session gnome-shell-extensions 
+baobab evince file-roller firefox 
+gedit gnome-backgrounds gnome-calculator gnome-control-center 
+gnome-screenshot gnome-system-monitor gnome-terminal gnome-tweak-tool 
+gstreamer0.10-plugins 
+mutter nautilus noise-player vala viewnior vlc budgie-desktop
+clutter-gst
+gst-plugins-bad
+gst-plugins-base
+gst-plugins-base-libs
+gst-plugins-good
+gstreamer
+totem
