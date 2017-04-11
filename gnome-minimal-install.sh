8c3dd1baad10ccc52f28d45d5fd89b8ac93d21af
revosftw <sahay.rohan@gmail.com>
2017-04-11 12:25:16 +0530

    inprogress

--- gnome-minimal-install.sh
+++ gnome-minimal-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo -e "Installing Gnome Minimal"
-pacstrap /mnt       file-roller gedit  gnome-calculator gnome-control-center gnome-screenshot gnome-system-monitor gnome-terminal gnome-tweak-tool mutter nautilus noise-player vala viewnior vlc 
-pacstrap /mnt baobab evince gdm gnome-backgrounds gnome-session gnome-shell-extensions gnome-themes-standard gvfs-mtp gvfs-nfs gvfs-smb xdg-user-dirs
+pacman -S xorg-server xorg-server-utils xorg-xinit xorg-xrandr xorg-drivers
+pacman -S file-roller gedit  gnome-calculator gnome-control-center gnome-screenshot gnome-system-monitor gnome-terminal gnome-tweak-tool mutter nautilus noise-player vala viewnior vlc 
+pacman -S baobab evince gdm gnome-backgrounds gnome-session gnome-shell-extensions gnome-themes-standard gvfs-mtp gvfs-nfs gvfs-smb xdg-user-dirs
