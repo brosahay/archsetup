b1174dc3e737034121a8ed11ffcbce9f3c5722d8
revosftw <sahay.rohan@gmail.com>
2017-06-19 19:13:31 +0530

    ongoing

---
+++ update-pacman-mirrorlist.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+curl -L "https://www.archlinux.org/mirrorlist/all/" --output /tmp/mirrorlist.new
+mv /tmp/mirrorlist.new /etc/pacman.d/mirrorlist
+reflector --verbose -l 50 --sort rate --save /etc/pacman.d/mirrorlist
